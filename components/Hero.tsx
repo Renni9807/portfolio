16f1bf0a58cc27dc353edd3217b141c3ca279e95
Renni9807 <practiceID98@gmail.com>
2024-07-27 00:56:17 +0800

    inital commit

---
+++ components/Hero.tsx
@@ -0,0 +1,53 @@
+import { Spotlight } from "./ui/Spotlight";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 
+        md:-left-32 md:-top-20 h-screen "
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full 
+            h-[80vh] w-[50vw] "
+          fill="pruple"
+        />
+        <Spotlight
+          className="top-28 left-80
+            h-[80vh] w-[50vh]"
+          fill="skyblue"
+        />
+      </div>
+      <div
+        className="h-[50rem] w-full dark:bg-black 
+      bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2]
+       relative flex items-center justify-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          className="absolute pointer-events-none inset-0
+         flex items-center justify-center dark:bg-black-100
+          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        ></div>
+        <p
+          className="text-4xl sm:text-7xl font-bold relative
+         z-20 bg-clip-text text-transparent bg-gradient-to-b 
+         from-neutral-200 to-neutral-500 py-8"
+        >
+          Backgrounds
+        </p>
+      </div>
+      <div className="flex justify-center relative my-20">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic web UI with spotlight
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
